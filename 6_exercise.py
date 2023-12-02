b746628a9c06271cdf5013f337a2319ff5b049b0
yungbrook <bruks.alex@gmail.com>
2023-12-02 19:01:57 +0100

    Update 6_exercise.py

--- 6_exercise.py
+++ 6_exercise.py
@@ -1,3 +1,15 @@
 # Display numbers divisible by 5 from a list
 #Iterate the given list of numbers and print only those numbers which are divisible by 5
 
+import random
+
+random.randint(0, 100)
+
+lst =[]
+
+for i in range(100):
+    lst.append(random.randint(0,100))
+
+
+
+lst
