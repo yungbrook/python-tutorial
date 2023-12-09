3e6d05d13a6059693d7e3c85d8ec43a2fb76e42b
yungbrook <bruks.alex@gmail.com>
2023-12-09 18:51:45 +0100

    Update 6_exercise.py

--- 6_exercise.py
+++ 6_exercise.py
@@ -16,3 +16,13 @@ cisla = [1,2,3,5,3,10,54,20,45,2,2,4]
 for cislo in cisla:
     if cislo % 5 == 0:
         print (cislo)
+
+
+nahodne_cele_cislo = random.randint (0,33)
+print(nahodne_cele_cislo)
+
+list_nahodnych_cisel = []
+for i in range(10):
+    list_nahodnych_cisel.append(random.randint (0,33))
+
+print(list_nahodnych_cisel)
