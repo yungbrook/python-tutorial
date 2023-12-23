8da1bbaa559624ab881238ba9be4deae4a1b049f
yungbrook <bruks.alex@gmail.com>
2023-12-23 17:48:17 +0100

    Create 8_exercse.py
    my real code

---
+++ 8_exercse.py
@@ -0,0 +1,14 @@
+str = "pynative"
+
+for i in range(len(str)):
+    if i % 2 == 1:
+        print(str[i])
+         
+         
+         
+         
+         
+         
+str = "pynative"       
+for i in range(1, len(str), 2):
+     print(str[i])
